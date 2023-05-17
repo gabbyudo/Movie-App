a66424078015c9fb912e3332fd60522fc47aed17
Udo Gabriel <gabbyy305400@gmail.com>
2023-05-17 19:53:43 +0100

    MovieApiServices built

--- app/src/main/java/com/coca/movieapp/MovieApiServices.kt
+++ app/src/main/java/com/coca/movieapp/MovieApiServices.kt
@@ -1,3 +1,33 @@
 package com.coca.movieapp
 
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = ""
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface RecipeApiService {
+    @GET("")
+    suspend fun getRecipe():
+            List<Movie>
+}
+object RecipeApi {
+    val retrofitService : RecipeApiService =
+        retrofit.create(RecipeApiService::class.java)
+
+}
+
