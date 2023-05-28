1502ae1f8ea4d280b8e0a6d42526e1c7b0f4ded1
Udo Gabriel <gabbyy305400@gmail.com>
2023-05-28 15:41:34 +0100

    Display hardcoded image using picasso

--- app/src/main/java/com/coca/movieapp/DetailAcitvity.kt
+++ app/src/main/java/com/coca/movieapp/DetailAcitvity.kt
@@ -2,15 +2,36 @@ package com.coca.movieapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.coca.movieapp.databinding.ActivityDetailBinding
+import com.squareup.picasso.Picasso
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var viewModel: DetailViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
         val movie: Int? = intent?.getIntExtra(EXTRA_MOVIE_ID, DEFAULT)
-        if (movie != null) { }
+
+            viewModel.movie.observe(this, Observer {
+               binding.title.text= it.title
+                binding.description.text = it.overview
+                binding.releaseDate.text = it.release_date
+                val image = binding.poster
+
+                Picasso.with(image.context)
+                    .load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXWI2scXOnCZyu63frAvg2P_V7cvaHYXKKTKYg0H4kNQ&s")
+                    .into(image)
+
+            })
+        viewModel.getMovies()
+
 
     }
     companion object {
--- app/src/main/java/com/coca/movieapp/DetailViewModel.kt
+++ app/src/main/java/com/coca/movieapp/DetailViewModel.kt
@@ -5,14 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DetailViewModel: ViewModel () {
-    private val _movies = MutableLiveData<Movies>()
-    val movies: LiveData<Movies>
-        get() = movies
+    private val _movie = MutableLiveData<Movies>()
+    val movie: LiveData<Movies>
+        get() = _movie
 
     fun getMovies() {
         val details = Movies(2, "good", "nm", "okey",
             "34",0.0, 0)
         val movie = details
-        _movies.value = movie
+        _movie.value = movie
+
     }
 }
